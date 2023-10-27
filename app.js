d080df9e6d4e7ea079da37d48c19bba68c501f48
melissabutler <melissaebutler2015@gmail.com>
2023-10-27 11:30:33 -0400

    completed exercise

---
+++ app.js
@@ -0,0 +1,43 @@
+console.log("Let's get this party started!");
+const $imgCache = $('#img-cache');
+
+
+
+// on pressing of the button, call the API
+$('#search-btn').on('click', async function(event) {
+    event.preventDefault();
+    console.log("search Button clicked");
+
+    const searchTerm = $('#search').val();
+    $('#search').val('');
+
+    const res = await axios.get("http://api.giphy.com/v1/gifs/search", {
+        params: {
+        q: searchTerm,
+        api_key: "3IVHxnUtpxmyoVbjip6tBgK0JSvamc2b"
+        }
+    });
+    appendGif(res.data);
+})
+
+// Add Gif to cache
+function appendGif(result){
+    const results = result.data.length;
+    if(results) {
+        const randomID = Math.floor(Math.random() * results);
+        const $newGif = $('<img>', {
+            src: result.data[randomID].images.original.url,
+        });
+    $newGif.addClass('col-3');
+    $newGif.addClass('img-fluid')
+    $imgCache.append($newGif);
+    }
+}
+
+//Remove Button
+$('#remove-btn').on('click', function(event){
+    event.preventDefault();
+    $('#img-cache').empty();
+})
+
+
